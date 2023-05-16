122afd8c79bf3b9268dbdf3c5fc71810820942f4
JairBarco <jair_barco@outlook.com>
2023-05-15 23:51:01 -0600

    Validaciones primera estación

--- estacion1.py
+++ estacion1.py
@@ -4,8 +4,10 @@ import time
 def main():
     # Definición de variables
     estacion_actual = 0
-    tiempos_reales = [60, 12, 12, 12, 12, 12, 12, 12, 25, 20, 60, 12, 12, 20, 25]  # Tiempos reales de las operaciones
-    tiempos_aleatorios = [52, 56, 12, 59, 15, 22, 52, 20, 31, 33, 23, 12, 14, 15, 53]  # Tiempos aleatorios de las operaciones
+    tiempos_reales = [6, 1, 1, 1, 1, 1, 1, 1, 2, 2, 6, 1, 1, 2, 2]  # Tiempos reales de las operaciones
+    tiempos_aleatorios = [5, 5, 1, 5, 1, 2, 5, 2, 3, 3, 2, 1, 1, 5, 5]  # Tiempos aleatorios de las operaciones
+    #tiempos_reales = [60, 12, 12, 12, 12, 12, 12, 12, 25, 20, 60, 12, 12, 20, 25]  # Tiempos reales de las operaciones
+    #tiempos_aleatorios = [52, 56, 12, 59, 15, 22, 52, 20, 31, 33, 23, 12, 14, 15, 53]  # Tiempos aleatorios de las operaciones
     operacion_actual = 0  # Variable que indica la operación que se está realizando
     operaciones_correctas = False  # Variable que indica si todas las operaciones fueron correctas
     DISENO_ESPALDA = "Diseño Espalda"
@@ -25,6 +27,7 @@ def main():
     DISENO_FINAL = "Diseño Final"
     operaciones = [DISENO_ESPALDA, LARGO_TALLE, CONTORNO_BUSTO, CONTORNO_CINTURA, CONTORNO_CADERA, ALTURA_CADERA, LARGO_MANGAS, LARGO_TOTAL_CAMISA, COLECCIONES, CREACION_COLOR, CREACION_TELA, TIPOS_TELAS, COLORES, CONSULTA_DISENO, DISENO_FINAL]
     estaciones = ["Estación 1", "Estación 2", "Estación 3", "Estación 4", "Estación 5", "Estación 6"]
+    prendas = [ {"diseno_espalda": "A", "largo_talle": 55, "contorno_busto": 115}]
 
     # Variables para almacenar el tiempo transcurrido en cada operación y el tiempo total
     tiempos_operacion = [0] * len(operaciones)
@@ -42,27 +45,98 @@ def main():
         tiempo_transcurrido = fin - inicio  # Tiempo transcurrido
         tiempos_operacion[operacion_actual] = tiempo_transcurrido  # Almacenar el tiempo transcurrido en la variable correspondiente
         print(f"Operación {operaciones[operacion_actual]} finalizada. Tiempo: {tiempo_transcurrido:.2f}s")
+        if operaciones[operacion_actual] == DISENO_ESPALDA:
+            # Actualizar el diseño de espalda en todas las prendas
+            for prenda in prendas:
+                prenda["diseno_espalda"] = "A"
+        elif operaciones[operacion_actual] == LARGO_TALLE:
+            # Actualizar el largo de talle en todas las prendas
+            for prenda in prendas:
+                prenda["largo_talle"] = 55
+        elif operaciones[operacion_actual] == CONTORNO_BUSTO:
+            # Actualizar el contorno de busto en todas las prendas
+            for prenda in prendas:
+                prenda["contorno_busto"] = 115
+    class validar_largo_talle:
+        def __init__(self, prendas, valor_definido):
+            self.prendas = prendas
+            self.valor_definido = valor_definido
+
+        def validar(self):
+            for prenda in self.prendas:
+                if prenda["largo_talle"] < self.valor_definido:
+                    return False
+            return True
+
+    class validar_diseno_espalda:
+        def __init__(self, prendas, diseno_referencia):
+            self.prendas = prendas
+            self.diseno_referencia = diseno_referencia
+
+        def validar(self):
+            for prenda in self.prendas:
+                if prenda["diseno_espalda"] != self.diseno_referencia:
+                    return False
+            return True
+
+    class validar_contorno_busto:
+        def __init__(self, prendas, valor_definido):
+            self.prendas = prendas
+            self.valor_definido = valor_definido
+
+        def validar(self):
+            for prenda in self.prendas:
+                if prenda["contorno_busto"] < self.valor_definido:
+                    return False
+            return True
 
     # Función para validar si las operaciones fueron correctas
-    def validar_operaciones():
-        # Realizar validación de las operaciones realizadas
-        # En este caso, simplemente se devuelve un valor aleatorio entre 0 y 1
-        # para simular si las operaciones fueron correctas o no
-        resultado_validacion = random.randint(0, 1)
+    def validar_operaciones(prendas):
+        resultado_validacion = False
+        validador = None
+        validadores = []
 
-        # Devolver True si las operaciones fueron correctas, False si no lo fueron
-        if resultado_validacion == 1:
-            print(f"Validación exitosa")
-        else:
-            print(f"Validación no exitosa")
-        return resultado_validacion == 1
+        # Validar cada operación según sus criterios de validación
+        if operaciones[operacion_actual] == DISENO_FINAL:
+            # Criterios de validación para la operación "Diseño Espalda"
+            validador = validar_diseno_espalda(prendas, "A")
+            print('Validacion 1')
+        if operaciones[operacion_actual] == DISENO_FINAL:
+            # Criterios de validación para la operación "Largo Talle"
+            validador = validar_largo_talle(prendas, 55)
+            print('Validacion 2')
+        if operaciones[operacion_actual] == DISENO_FINAL:
+            # Criterios de validación para la operación "Contorno Busto"
+            validador = validar_contorno_busto(prendas, 115)
+            print(prendas)
+            print('Validacion 3')
 
+            # Realizar todas las validaciones
+        for validador in validadores:
+            resultado_validacion = validador.validar()
+            if not resultado_validacion:
+                break
+
+        # Llama al método validar() en el objeto validador y asigna el resultado a la variable resultado_validacion
+        if validador is not None:
+            # Llama al método validar() en el objeto validador y asigna el resultado a la variable resultado_validacion
+            resultado_validacion = validador.validar()
+
+            # Imprimir el mensaje de validación correspondiente
+            if resultado_validacion:
+                print(f"Validación de {operaciones[operacion_actual]} exitosa")
+                resultado_validacion = True
+            else:
+                print(f"Validación de {operaciones[operacion_actual]} no exitosa")
+                resultado_validacion = False
+
+        return resultado_validacion
     # Función para avanzar a la siguiente estación
     def avanzar_a_siguiente_estacion():
-        actual_estacion = 0
-        actual_estacion += 1
-        estacion_actual = actual_estacion
-        if actual_estacion == len(estaciones):
+        global estacion_actual # Indicar que estacion_actual se modificará en el ámbito superior
+        estacion_actual = 0
+        estacion_actual += 1
+        if estacion_actual == len(estaciones):
             estacion_actual = 0
         print(f"Avanzando a la siguiente estación: {estaciones[estacion_actual]}")
 
@@ -72,20 +146,22 @@ def main():
         tiempo_operacion = random.uniform(tiempos_aleatorios[operacion_actual], tiempos_reales[operacion_actual])
         realizar_operacion(tiempo_operacion)
 
-        # Verificar si la operación actual es la última
-        if operacion_actual == len(operaciones) - 1:
-            # Validar operaciones
-            operaciones_correctas = validar_operaciones()
+        # Si no es la última operación, avanzar a la siguiente
+        if operacion_actual < len(operaciones) - 1:
+            operacion_actual += 1
+
+        # Verificar si todas las operaciones han sido completadas
+        if operacion_actual == 14:
+            #Validar operaciones
+            operaciones_correctas = validar_operaciones(prendas)
 
             # Si las operaciones fueron correctas, avanzar a la siguiente estación
             if operaciones_correctas:
                 avanzar_a_siguiente_estacion()
-            # Si las operaciones fueron incorrectas, volver a la operación inicial
-            else:
-                operacion_actual = 0
-        # Si no es la última operación, avanzar a la siguiente
-        else:
-            operacion_actual += 1
+
+        # Si la operación actual es la última y todas las operaciones fueron completadas, romper el bucle y finalizar la simulación
+        if operacion_actual == len(operaciones) - 1 and operaciones_correctas:
+            break
 
 if __name__ == '__main__':
     main()
